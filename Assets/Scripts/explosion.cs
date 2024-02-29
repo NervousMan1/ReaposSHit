fcf2ddaadbe83c1bafe83a604ab4977ea10831b8
Nikita Gribunin <15lonnik14@gmail.com>
2024-02-29 20:03:51 +0400

    нипанимаю, всё сламалась

--- Assets/Scripts/explosion.cs
+++ Assets/Scripts/explosion.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Principal;
 using UnityEngine;
 
-public class explosion : MonoBehaviour
+public class Explosion : Health
 {
     Collider2D[] inExplosionRadius = null;
-    [SerializeField] private int explosionDamage = 2; 
+    [SerializeField] private int explosionDamage = 2;
     [SerializeField] private float explosionRadius = 5f;
+    [SerializeField] private GameObject explosionEffect;
 
 
     public void Explode()
     {
+        Instantiate(explosionEffect, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1f), Quaternion.identity );
         inExplosionRadius = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
         foreach (Collider2D col in inExplosionRadius)
         {
-            col.GetComponent<Health>().TakeDamage(explosionDamage);
+            if(col.gameObject.TryGetComponent(out Health damaged))
+            damaged.TakeDamage(explosionDamage);
         }
+        Destroy(gameObject);
     }
 
-    private void OnDrawGizmos()
+    protected override void Kill()
     {
-       Gizmos.color = Color.green;
-       Gizmos.DrawWireSphere(transform.position, explosionRadius);
+        base.Kill();
+        Explode();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
